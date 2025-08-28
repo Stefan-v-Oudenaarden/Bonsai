e31414c2a67fc52b13d1f6adc9469e5b53d844e7
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2025-08-28 22:06:45 +0200

    New layout.

---
+++ Blazor WASM/GlobalUsings.cs
@@ -0,0 +1,9 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+
+global using BonsaiGenerators;
+global using BonsaiGenerators.Content;
+global using BonsaiGenerators.Tables;
