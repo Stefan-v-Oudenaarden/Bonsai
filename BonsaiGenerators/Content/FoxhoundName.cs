79a343c99c02a92c33efda4e443486db9e8918a8
Stefan van Oudenaarden <s.oudenaarden@gmail.com>
2025-09-01 22:19:34 +0200

    Generator: Foxhound Unit Codenames

---
+++ BonsaiGenerators/Content/FoxhoundName.cs
@@ -0,0 +1,16 @@
+﻿using BonsaiGenerators.Tables.Generic.Creatures;
+using BonsaiGenerators.Tables.Misc;
+
+namespace BonsaiGenerators.Content
+{
+    public class FoxhoundNameGenerator
+    {
+        private static readonly FoxhoundAdjective Adjective = new();
+        private static readonly AnimalsSingleWord Animal = new();
+
+        public string Next()
+        {
+            return $"{Adjective} {Animal}";
+        }
+    }
+}
